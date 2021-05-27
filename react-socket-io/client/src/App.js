b048aba0def679909d7c666d7801326ba75cad00
Vinh Ho <vinhhofb@gmail.com>
2021-05-27 14:54:03 +0700

    n

--- react-socket-io/client/src/App.js
+++ react-socket-io/client/src/App.js
@@ -4,12 +4,13 @@ import io from "socket.io-client"
 import "./App.css"
 
 function App() {
-	const [ state, setState ] = useState({ message: "", name: "" })
-	const [ chat, setChat ] = useState([])
+  const [ state, setState ] = useState({ message: "", name: "" })
+  const [nameSubmit, setNameSubmit] = useState(false)
+  const [nameUser, setNameUser]=useState("")
+  const [ chat, setChat ] = useState([])
 
-	const socketRef = useRef()
-
-	useEffect(
+  const socketRef = useRef()
+  useEffect(
 		() => {
 			socketRef.current = io.connect("http://localhost:4000")
 			socketRef.current.on("message", ({ name, message }) => {
@@ -20,11 +21,22 @@ function App() {
 		[ chat ]
 	)
 
-	const onTextChange = (e) => {
-		setState({ ...state, [e.target.name]: e.target.value })
-	}
+  const onNameChange = (e) =>{
+    setNameUser(e.target.value)
+  }
+  const nameSubmitForm =()=>{
+    setNameSubmit(true)
+  }
 
-	const onMessageSubmit = (e) => {
+  const onContentChange =(e) =>{
+    const getNameUser =nameUser;
+    console.log(e.target.value)
+    
+    setState({ ...state, name: getNameUser , message: e.target.value })
+    console.log(state)
+  }
+
+  const onMessageSubmit = (e) => {
 		const { name, message } = state
 		socketRef.current.emit("message", { name, message })
 		e.preventDefault()
@@ -34,38 +46,66 @@ function App() {
 
 	const renderChat = () => {
 		return chat.map(({ name, message }, index) => (
-			<div key={index}>
-				<h3>
-					{name}: <span>{message}</span>
-				</h3>
-			</div>
+       nameUser == name ? (
+        <div>
+        <div className="bg1 mb-2 p-2 float-left box-messenger">
+          <p className="mb-0 text-messenger text-white">{message} </p>
+        </div>
+        <div style={{clear: 'both'}} />
+      </div>
+       ):(
+        <div>
+        <div className="bg2 mb-2 p-2 float-right box-messenger">
+          <p className="mb-0 text-messenger">{message}</p>
+        </div>
+        <div style={{clear: 'both'}} />
+      </div>
+       )
+			
 		))
 	}
 
-	return (
-		<div className="card">
-			<form onSubmit={onMessageSubmit}>
-				<h1>Messenger</h1>
-				<div className="name-field">
-          <input name="name" onChange={(e) => onTextChange(e)} value={state.name} placeholder="name"></input>
-					
-				</div>
-				<div>
-          <input name="message"
-						onChange={(e) => onTextChange(e)}
-						value={state.message}
-						id="outlined-multiline-static"
-						variant="outlined"
-						label="Message"></input>
-				</div>
-				<button>Send Message</button>
-			</form>
-			<div className="render-chat">
-				<h1>Chat Log</h1>
-				{renderChat()}
-			</div>
-		</div>
-	)
+
+
+  
+
+  return (
+    nameSubmit ? (
+      <div className="box-main">
+    <div className="bg1 p-3 box-header">
+      <p className="font-weight-bold text-white mb-0">{nameUser}</p>
+    </div>
+    <div className="p-2 box-content">
+    {renderChat()}
+      
+      
+      
+    </div>
+    <form onSubmit={onMessageSubmit}>
+    <div className="pl-2 pt-2 box-bottom">
+    
+      <div className="box-input">
+        <input onChange={(e) => onContentChange(e)} value={state.message} name="message"
+            placeholder="Please a reply" 
+            className="bg2 pl-2 input-text" 
+            type="text" />
+      </div>
+      <div className="px-2 box-button">
+        <button className="bg1 text-center reply-button">
+          <i className="fa fa-paper-plane text-white button-icon" aria-hidden="true" />
+        </button>
+      </div>
+      
+    </div>
+    </form>
+  </div>
+    ):(
+      <form onSubmit={nameSubmitForm}>
+        <input name="name" onChange={(e) => onNameChange(e)} value={nameUser.name}></input>
+        <button>vvcvc</button>
+      </form>
+    )
+  )
 }
 
 export default App
