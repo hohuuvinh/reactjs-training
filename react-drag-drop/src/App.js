3f81bbbea160f38f1282415c346dc1e4e21c66ed
Vinh Ho <vinhhofb@gmail.com>
2021-06-28 09:33:56 +0700

    update crud firebase

--- react-drag-drop/src/App.js
+++ react-drag-drop/src/App.js
@@ -1,22 +1,22 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 
-const getItems =()=> [{
-  id:"1",
-  content:"vvvvvb"
-},{
-  id:"2",
-  content:"gf"
-},{
-  id:"3",
-  content:"n"
-},{
-  id:"4",
-  content:"nb"
-},{
-  id:"5",
-  content:"bvvbbvb"
+const getItems = () => [{
+  id: "1",
+  image: "https://mediadesknm.com/wp-content/uploads/2018/09/photographer-698908_960_720.jpg"
+}, {
+  id: "2",
+  image: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+}, {
+  id: "3",
+  image: "https://www.chinaairlines-vn.com/uploads/data/images/tong-hop-kinh-nghiem-du-lich-los-angeles-tu-tuc-gia-re-1.jpg"
+}, {
+  id: "4",
+  image: "https://imgcy.trivago.com/c_lfill,d_dummy.jpeg,e_sharpen:60,f_auto,h_450,q_auto,w_450/itemimages/13/32/1332344_v4.jpeg"
+}, {
+  id: "5",
+  image: "https://s7d2.scene7.com/is/image/ritzcarlton/RCLOSAN_00052_conversion?$SmallViewport100pct$"
 }]
 
 // a little function to help us with reordering the result
@@ -26,86 +26,78 @@ const reorder = (list, startIndex, endIndex) => {
   result.splice(endIndex, 0, removed);
   return result;
 };
-const grid = 10;
+
 const getItemStyle = (isDragging, draggableStyle) => ({
   // some basic styles to make the items look a bit nicer
-
-  padding: '10px',
-  margin: `0 ${grid}px 0 0`,
-  // change background colour if dragging
-  background: isDragging ? 'lightgreen' : 'grey',
+  width: "80px",
+  height: "80px",
+  marginRight: '10px',
   // styles we need to apply on draggables
   ...draggableStyle,
 });
 
 const getListStyle = isDraggingOver => ({
   background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  border: isDraggingOver ? '2px dotted blue' : '2px solid lightgrey',
   display: 'flex',
-  padding: grid,
+  padding: '10px',
   overflow: 'auto',
 });
 
 function App() {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     items: getItems(6),
-  //   };
-  //   this.onDragEnd = this.onDragEnd.bind(this);
-  // }
-  const [state,setState] = useState({items: getItems()})
-console.log(state)
+  const [state, setState] = useState({ items: getItems() })
+  
+
+  var ArrSendServer = [];
+  for (let i = 0; i < state.items.length; i++) {
+    ArrSendServer.push({id:state.items[i].id,image:state.items[i].image,index:i});
+  }
+  console.log(ArrSendServer)
   function onDragEnd(result) {
-    // dropped outside the list
     if (!result.destination) {
       return;
     }
-
     const items = reorder(
       state.items,
       result.source.index,
       result.destination.index
     );
     setState({ items });
-  }
 
+  }
   // Normally you would want to split things out into separate components.
   // But in this example everything is just done in one place for simplicity
-  
-    return (
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable" direction="horizontal">
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-              {...provided.droppableProps}
-            >
-              {state.items.map((item, index) => (
-                
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    );
-  }
-
-
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable" direction="horizontal">
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+            {...provided.droppableProps}
+          >
+            {state.items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}
+                  >
+                    <img src={item.image} width="100%" height="100%"></img>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
 export default App;
